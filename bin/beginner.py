318c46fbd5c1a226b818b17edabe32748b0c579c
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-30 11:49:21 +0100

    Created bin/beginner.py

---
+++ bin/beginner.py
@@ -0,0 +1,19 @@
+"""beginner
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-12-30"
+
+class beginner:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(beginner.beginner())
+	
+	def beginner():
+		"""beginner first funtion
+		"""
+		return "beginner" #for the test (see test_beginner.py)
+		
+if __name__ == "__main__":
+	beginner()
