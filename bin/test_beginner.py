f301a4205f962b9e7d515acc9d23e9ac2ee7e964
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-30 11:49:24 +0100

    Created bin/test_beginner.py

---
+++ bin/test_beginner.py
@@ -0,0 +1,17 @@
+"""Test beginner file
+"""
+from beginner import *
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-12-30"
+
+def test():
+	"""Tests the beginner function in the beginner class
+	Write here all test you want to do.
+	REMEMBER to test your programm you can't use __init__ function
+	"""
+	assert beginner.beginner() == "beginner", "test failed"
+	#assert beginner.<function>(<values>) == <the result(s) you would like to have>, "<the fail message>"
+	
+if __name__ == "__main__":
+	test()
